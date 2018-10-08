734e86ed20f82db00e2efce3388766c3496a4352
ahabowski <43949330+ahabowski@users.noreply.github.com>
2018-10-08 18:29:52 +0200

    Update zad2.java

---
+++ zad2.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a = 1;
+		int b = 13;
+		int c = 2;
+		int s = 0;
+		if (a==13){
+			System.out.println(s);
+		}else if(a!=13 && b==13){
+			s=a;
+			System.out.println(s);
+		}else if(a!=13 && b!=13 && c==13){
+			s=a+b;
+			System.out.println(s);
+		}else if(a!=13 && b!=13 && c!=13){
+			s=a+b+c;
+			System.out.println(s);
+		}
+		
+		
+	}
+}
